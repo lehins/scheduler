195e1d8c9d949c674fde08bd2955fee868941ef2
Alexey Kuleshevich <alexey@kuleshevi.ch>
2019-03-28 22:41:29 +0300

    Adjust streamly benchmarks. Add shceduler benchmark that compares directly to streamly. Add benchmarks that discard results

--- scheduler/bench/Scheduler.hs
+++ scheduler/bench/Scheduler.hs
@@ -1,9 +1,11 @@
+{-# LANGUAGE BangPatterns #-}
 module Main where
 
+import Control.Monad (void)
 import Control.Concurrent.Async (mapConcurrently, mapConcurrently_)
 import Control.Monad.Par (parMapM, runParIO)
 import Control.Parallel (par)
-import Control.Scheduler (Comp(Par), traverseConcurrently, traverseConcurrently_)
+import Control.Scheduler
 import Criterion.Main
 import Data.Foldable as F
 import UnliftIO.Async (pooledMapConcurrently, pooledMapConcurrently_)
@@ -12,30 +14,50 @@ import qualified Streamly.Prelude as S
 
 
 main :: IO ()
-main = defaultMain (mkSumBench 1000)
+main = defaultMain (mkSumBench 1000 100000)
 
-mkSumBench :: Int -> [Benchmark]
-mkSumBench n =
+mkSumBench :: Int -> Int -> [Benchmark]
+mkSumBench n elts =
   [ env (pure ls) $ \xs ->
       bgroup
         ("Sums: " <> show n)
         [ bgroup "unliftio" [bench "pooledMapConcurrently" $ nfIO (pooledMapConcurrently f xs)]
         , bgroup "monad-par" [bench "parMapM" $ nfIO (runParIO $ parMapM f xs)]
         , bgroup "scheduler" [bench "traverseConcurrently" $ nfIO (traverseConcurrently Par f xs)]
+        , bgroup "streamly" [bench "mapM" $ nfIO $ S.runStream $ asyncly $ S.mapM f $ S.fromList xs]
         , bgroup "async" [bench "mapConcurrently" $ nfIO (mapConcurrently f xs)]
         , bgroup "base" [bench "traverse . par" $ nfIO (traverse fpar xs)]
         , bgroup "base" [bench "traverse . seq" $ nfIO (traverse f xs)]
         ]
-  , bench "streamly" $ nfIO $
-        S.runStream $ asyncly $
-            S.replicateM n (fstreamly $ S.enumerateFromTo 0 (100000 :: Int))
+  , env (pure ls) $ \xs ->
+      bgroup
+        ("Discard Sums: " <> show n)
+        [ bgroup "unliftio" [bench "pooledMapConcurrently_" $ nfIO (pooledMapConcurrently_ f xs)]
+        , bgroup "scheduler" [bench "traverseConcurrently_" $ nfIO (traverseConcurrently_ Par f xs)]
+        , bgroup "async" [bench "mapConcurrently" $ nfIO (mapConcurrently_ f xs)]
+        ]
+  , bgroup
+      "NoList"
+      [ bench "scheduler" $
+        nfIO $ withScheduler_ Par $ \s -> loopM_ n $ scheduleWork s $ void $ f [0 .. elts]
+      , bench "streamly" $
+        nfIO $ S.runStream $ asyncly $ S.replicateM n (fstreamly $ S.enumerateFromTo 0 elts)
+      ]
   ]
   where
-    ls = replicate n [0 .. 100000] :: [[Int]]
+    ls = replicate n [0 .. elts] :: [[Int]]
     f xs =
       let ys = F.foldl' (+) 0 xs
        in ys `seq` pure ys
     fpar xs =
       let ys = F.foldl' (+) 0 xs
        in ys `par` pure ys
-    fstreamly xs = S.foldl' (+) 0 xs
+    fstreamly = S.foldl' (+) 0
+
+
+loopM_ :: Monad m => Int -> m a -> m ()
+loopM_ n f = go 0
+  where
+    go !i
+      | i < n = f >> go (i + 1)
+      | otherwise = pure ()
