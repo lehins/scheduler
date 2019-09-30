8921bb654559d2543c5d56d189fd968fe69c7059
Alexey Kuleshevich <alexey@kuleshevi.ch>
2019-09-30 03:13:57 +0300

    Remove fib as a dependency, will need to replace ot with something better in benchmarks

--- scheduler/bench/Scheduler.hs
+++ scheduler/bench/Scheduler.hs
@@ -2,38 +2,38 @@ module Main where
 
 import qualified Control.Concurrent.Async as A (mapConcurrently, replicateConcurrently)
 import Control.Monad (replicateM)
-import Control.Monad.Par (IVar, Par, get, newFull_, parMapM, runParIO)
+import Control.Monad.Par (IVar, Par, get, newFull_, runParIO) --, parMapM)
 import Control.Parallel (par)
 import Control.Scheduler
 import Criterion.Main
 import Control.DeepSeq
 import Data.Foldable as F
 import Data.IORef
-import Fib
+--import Fib
 import Streamly (asyncly)
 import qualified Streamly.Prelude as S
 import UnliftIO.Async (pooledMapConcurrently, pooledReplicateConcurrently)
 
 main :: IO ()
-main = do
+main =
   defaultMain
-    ([mkBenchReplicate "Fib" n x fibIORef fibParVar | n <- [1000], x <- [10000]] ++
+    (--[mkBenchReplicate "Fib" n x fibIORef fibParVar | n <- [1000], x <- [10000]] ++
      [mkBenchReplicate "Sum" n x sumIORef sumParVar | n <- [1000], x <- [1000]] ++
-     [mkBenchMap "Fib" n fibIO fibParIO fibPar | n <- [2000]] ++
+     --[mkBenchMap "Fib" n fibIO fibParIO fibPar | n <- [2000]] ++
      [mkBenchMap "Sum" n sumIO sumParIO sumPar | n <- [2000]])
   where
-    fibIO :: Int -> IO Integer
-    fibIO x = do
-      let y = fib $ toInteger x
-      y `seq` pure y
-    fibParIO :: Int -> IO Integer
-    fibParIO x = do
-      let y = fib $ toInteger x
-      y `par` pure y
-    fibPar :: Int -> Par Integer
-    fibPar x = do
-      let y = fib $ toInteger x
-      y `seq` pure y
+    -- fibIO :: Int -> IO Integer
+    -- fibIO x = do
+    --   let y = fib $ toInteger x
+    --   y `seq` pure y
+    -- fibParIO :: Int -> IO Integer
+    -- fibParIO x = do
+    --   let y = fib $ toInteger x
+    --   y `par` pure y
+    -- fibPar :: Int -> Par Integer
+    -- fibPar x = do
+    --   let y = fib $ toInteger x
+    --   y `seq` pure y
     sumIO :: Int -> IO Int
     sumIO x = do
       let y = F.foldl' (+) 0 [x .. 100*x]
@@ -46,10 +46,10 @@ main = do
     sumPar x = do
       let y = F.foldl' (+) 0 [x .. 100*x]
       y `seq` pure y
-    fibIORef :: IORef Int -> IO Integer
-    fibIORef xRef = readIORef xRef >>= fibIO
-    fibParVar :: IVar Int -> Par Integer
-    fibParVar ivar = get ivar >>= fibPar
+    -- fibIORef :: IORef Int -> IO Integer
+    -- fibIORef xRef = readIORef xRef >>= fibIO
+    -- fibParVar :: IVar Int -> Par Integer
+    -- fibParVar ivar = get ivar >>= fibPar
     sumIORef :: IORef Int -> IO Int
     sumIORef xRef = readIORef xRef >>= sumIO
     sumParVar :: IVar Int -> Par Int
